ba4bd2a3009103e04198bf7f245dce4112a6b15a
Vachek-hooves <vachek0602@gmail.com>
2025-02-04 12:51:08 +0200

    create tab bar

--- screen/menu/TabMenuNav.js
+++ screen/menu/TabMenuNav.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Mood, Saves, Statistics} from '../tab';
 
@@ -6,14 +6,78 @@ const Tab = createBottomTabNavigator();
 
 const TabMenuNav = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Mood" component={Mood} />
-      <Tab.Screen name="Saves" component={Saves} />
-      <Tab.Screen name="Statistics" component={Statistics} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'white',
+          borderRadius: 30,
+          position: 'absolute',
+          bottom: 20,
+          left: 20,
+          right: 20,
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 10,
+          marginHorizontal: 40,
+        },
+        tabBarShowLabel: false,
+      }}>
+      <Tab.Screen
+        name="Statistics"
+        component={Statistics}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/image/tabBar/statistics.png')}
+              style={[
+                styles.tabIcon,
+                {tintColor: focused ? '#FF64FF' : '#666'},
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Mood"
+        component={Mood}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/image/tabBar/mood.png')}
+              style={[
+                styles.tabIcon,
+                {tintColor: focused ? '#FF64FF' : '#666'},
+              ]}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Saves"
+        component={Saves}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={require('../../assets/image/tabBar/saves.png')}
+              style={[
+                styles.tabIcon,
+                {tintColor: focused ? '#FF64FF' : '#666'},
+              ]}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 export default TabMenuNav;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabIcon: {
+    width: 32,
+    height: 32,
+    resizeMode: 'contain',
+    marginTop: 10,
+  },
+});
