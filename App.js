0029585d3d66fb2c333712db7de2d71ab9117c11
Vachek-hooves <vachek0602@gmail.com>
2025-02-04 10:18:38 +0200

    init Welcome

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import {StoreProvider} from './store/context';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {Welcome} from './screen/stack';
 import TabMenuNav from './screen/menu/TabMenuNav';
+import Introduction from './screen/stack/Introduction';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Introduction" component={Introduction} />
           <Stack.Screen name="TabMenuNav" component={TabMenuNav} />
         </Stack.Navigator>
       </NavigationContainer>
--- screen/stack/Welcome.js
+++ screen/stack/Welcome.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {useEffect} from 'react';
 
-const Welcome = () => {
+const Welcome = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Introduction');
+    }, 1500);
+  }, []);
+
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+    <Image
+      style={{width: '100%', height: '100%'}}
+      source={require('../../assets/image/loader/loader.png')}
+    />
+  );
+};
 
-export default Welcome
+export default Welcome;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
