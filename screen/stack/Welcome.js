80571af18d2ddfa9f3f794cb5955981773f4970a
Vachek-hooves <vachek0602@gmail.com>
2025-02-10 09:22:42 +0200

    update

--- screen/stack/Welcome.js
+++ screen/stack/Welcome.js
@@ -1,18 +1,22 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View, ImageBackground} from 'react-native';
 import {useEffect} from 'react';
 
 const Welcome = ({navigation}) => {
-  useEffect(() => {
-    setTimeout(() => {
-      navigation.navigate('Introduction');
-    }, 1500);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigation.navigate('Introduction');
+  //   }, 1500);
+  // }, []);
 
   return (
-    <Image
+    <ImageBackground
       style={{width: '100%', height: '100%'}}
-      source={require('../../assets/image/loader/loader.png')}
-    />
+      source={require('../../assets/image/bg/bg.png')}>
+      <Image
+        source={require('../../assets/image/logo/logo.png')}
+        style={{marginTop: '20%'}}
+      />
+    </ImageBackground>
   );
 };
 
